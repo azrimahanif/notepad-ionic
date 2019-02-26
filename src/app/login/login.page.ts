8af5ff2901a08f260904f67c7f0f108c5d25c638
mobaproj2019 <mobaproj2019@gmail.com>
2019-02-26 21:12:59 +0800

    initial commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {HomePage} from '../home/home.page';
+import { NavController } from "@ionic/angular";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
